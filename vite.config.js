ba2c8efd5ddd1ae068cd345e141104e96da0936d
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-09-28 17:59:56 +0300

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VedatZeybek.github.io/' // GitHub repo adın
+  base: './'  // repo adını yaz
 })
