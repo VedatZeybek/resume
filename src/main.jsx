a62eebf44b0961fb86a9eefe8728a3b14e038b04
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-09-30 10:19:47 +0300

    m

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App.jsx'
-import Projects from './Projects.jsx'
-import Navbar from './Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
